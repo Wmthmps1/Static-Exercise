4462f96d2e8c2ebe0ad74c687373fa2c561097b7
William Thompson <wmthmps1@gmail.com>
2023-12-22 21:44:16 -0600

    Initial Commit

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -1,10 +1,58 @@
-﻿namespace StaticExercise
+﻿using System.Security.Principal;
+
+namespace StaticExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Would you like to convert Celsius to Fahrenheit or Fahrenheit to Celsius?");
+            Console.WriteLine("Enter 1 for Celsius to Fahrenheit and 2 for Fahrenheit to Celsius");
+            bool validInput = int.TryParse(Console.ReadLine(), out int userInput);
+
+            while (userInput != 1 && userInput != 2 || validInput == false)
+            {
+                Console.WriteLine("Invalid Input");
+                Console.WriteLine("Type 1 for Yes and 2 for No");
+                validInput = int.TryParse(Console.ReadLine(), out userInput);
+            }
+
+            if (userInput == 1)
+            {
+                Console.WriteLine("Enter your value in Celsius to convert to Fahrenheit");
+                validInput = double.TryParse(Console.ReadLine(), out double userTemp);
+
+                while (validInput == false)
+                {
+                    Console.WriteLine("Invalid Input");
+                    Console.WriteLine("Enter your value in Celsius to convert to Fahrenheit");
+                    validInput = double.TryParse(Console.ReadLine(), out userTemp);
+                }
+
+                double newTemp = TempConverter.CelsiusToFahrenheit(userTemp);
+                Console.WriteLine($"{userTemp} degrees Celsius is {newTemp} degrees Fahrenheit");
+
+            }
+            else if (userInput == 2)
+            {
+                Console.WriteLine("Enter your value in Fahrenheit to convert to Celsius");
+                validInput = double.TryParse(Console.ReadLine(), out double userTemp);
+
+                while (validInput == false)
+                {
+                    Console.WriteLine("Invalid Input");
+                    Console.WriteLine("Enter your value in Fahrenheit to convert to Celsius");
+                    validInput = double.TryParse(Console.ReadLine(), out userTemp);
+                }
+
+                double newTemp = TempConverter.FahrenheitToCelsius(userTemp);
+                Console.WriteLine($"{userTemp} degrees Fahrenheit is {newTemp} degrees Celsius");
+            }
+            else
+            {
+
+            }
         }
     }
 }
+
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace StaticExercise
+{
+	public static class TempConverter
+	{
+		public static double FahrenheitToCelsius(double temperature)
+		{
+            // °C = (°F - 32) × 5/9
+            temperature = (temperature - 32.0) * (5.0 / 9.0);
+            return temperature;
+		}
+
+        public static double CelsiusToFahrenheit(double temperature)
+        {
+
+            // °F = (°C × 9/5) + 32
+            temperature = (temperature * (9.0 / 5.0)) + 32.0;
+            return temperature;
+        }
+
+    }
+}
+
